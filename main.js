a3c92141362aaa03dfd4ea5a01afacb533a5a241
MillerRabin <millerrabin@raintech.su>
2019-02-28 17:18:20 +0300

    update deps

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-const intensionServer = require('./intensionServer.js');
+const { IntentionStorage } = require('intention-storage');
 
+const intentionStorage = new IntentionStorage();
+const storageServer = intentionStorage.createServer({ address: 'localhost' });
+
+console.log(`Server is listening at ${storageServer.port}`);
+
+intentionStorage.createIntention({
+    title: {
+        en: 'Need possibility to get structurized user input',
+        ru: 'Нужна возможность принимать структурированный пользовательский ввод'
+    },
+    input: 'Recognition',
+    output: 'Entities',
+    onData: async (status, intention, value) => {
+        if (status == 'data')
+            console.log(value);
+        if (status == 'error')
+            console.log(value);
+    }
+});
